5539f77b526270837df0e625bff4f14f663b8090
Vinícius Sousa <vinicius.antunes@proton.me>
2024-06-14 19:58:20 -0300

    working on cartpole irl agent

---
+++ cartpole_agent.py
@@ -0,0 +1,102 @@
+import gymnasium as gym
+import numpy as np
+import math
+
+class CartPoleQLearning:
+    def __init__(self, buckets=(1, 1, 6, 12), num_episodes=500, min_lr=0.1, min_epsilon=0.1, discount=1.0, decay=25):
+        self.buckets = buckets
+        self.num_episodes = num_episodes
+        self.min_lr = min_lr
+        self.min_epsilon = min_epsilon
+        self.discount = discount
+        self.decay = decay
+        self.reward = 0
+        self.obs = None
+
+        self.env = gym.make('CartPole-v1')
+
+        # This is the action-value function being initialized to 0
+        self.Q_table = np.zeros(self.buckets + (self.env.action_space.n,))
+
+    def discretize(self, obs):
+        upper_bounds = [self.env.observation_space.high[0], 0.5, self.env.observation_space.high[2], math.radians(50)]
+        lower_bounds = [self.env.observation_space.low[0], -0.5, self.env.observation_space.low[2], -math.radians(50)]
+        ratios = [(obs[i] + abs(lower_bounds[i])) / (upper_bounds[i] - lower_bounds[i]) for i in range(len(obs))]
+        new_obs = [int(round((self.buckets[i] - 1) * ratios[i])) for i in range(len(obs))]
+        new_obs = [min(self.buckets[i] - 1, max(0, new_obs[i])) for i in range(len(obs))]
+        return tuple(new_obs)
+
+    def choose_action(self, state, epsilon):
+        return self.env.action_space.sample() if (np.random.random() <= epsilon) else np.argmax(self.Q_table[state])
+
+    def update_q(self, state_old, action, reward, state_new, alpha):
+        self.Q_table[state_old][action] += alpha * (reward + self.discount * np.max(self.Q_table[state_new]) -
+                                                    self.Q_table[state_old][action])
+
+    def get_epsilon(self, t):
+        return max(self.min_epsilon, min(1., 1. - math.log10((t + 1) / self.decay)))
+
+    def get_alpha(self, t):
+        return max(self.min_lr, min(1., 1. - math.log10((t + 1) / self.decay)))
+
+    def get_feature_expectation(self, gamma=0.99, num_episodes=100):
+        feature_expectation = np.zeros(4)
+        for _ in range(num_episodes):
+            done = False
+            current_state = self.discretize(self.env.reset()[0])
+            t = 0
+            while not done:
+                action = self.choose_action(current_state, 0)
+                obs, reward, done, _, _ = self.env.step(action)
+                current_state = self.discretize(obs)
+                sigmoid_obs = 1 / (1 + np.exp(-obs) + 1e-6)
+                feature_expectation += sigmoid_obs*(gamma**t)
+                t += 1
+        return feature_expectation / num_episodes
+
+    def get_reward(self):
+        return self.reward
+    def run(self):
+        for e in range(self.num_episodes):
+            # As states are continuous, discretize them into buckets
+
+            current_state = self.discretize(self.env.reset()[0])
+
+            # Get adaptive learning alpha and epsilon decayed over time
+            alpha = self.get_alpha(e)
+            epsilon = self.get_epsilon(e)
+
+            done = False
+            while not done:
+                # Choose action according to greedy policy and take it
+                action = self.choose_action(current_state, epsilon)
+                self.obs, self.reward, done, _, _ = self.env.step(action)
+                new_state = self.discretize(self.obs)
+
+                # Update Q-Table
+                self.update_q(current_state, action, self.get_reward(), new_state, alpha)
+                current_state = new_state
+
+
+    def play(self):
+        self.env = gym.make('CartPole-v1')
+        t = 0
+        done = False
+        current_state = self.discretize(self.env.reset())
+        while not done:
+            self.env.render()
+            t = t+1
+            # Select action from Q table
+            action = self.choose_action(current_state, 0)
+            obs, reward, done, _ = self.env.step(action)
+            new_state = self.discretize(obs)
+            current_state = new_state
+
+        return t
+
+
+
+if __name__ == '__main__':
+    agent = CartPoleQLearning()
+    agent.run()
+    agent.play()
--- irl2.py
+++ irl2.py
@@ -1,32 +1,54 @@
-from typing import Iterable
-
-import agent as agents
-import numpy
-import torch
+import gymnasium as gym
+import numpy as np
+import cartpole_agent
 
 
-def get_expert_feature_expectations(expert=agents.QLearningAgent(), env=agents.Game(), N=100):
-    expectation = numpy.zeros(expert.num_states)
-    for _ in range(N):
-        state = env.reset()
-        done = False
-        while not done:
-            action = expert.get_action(state)
-            state, reward, done, _ = env.step(action)
-            expectation += expert.get_features(state)
+class IrlAgent(cartpole_agent.CartPoleQLearning):
+    def __init__(self, expert_feature_expectation, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.expert_feature_expectation = expert_feature_expectation
+        self.weights = np.random.rand(4)
+        self.feature_expectations = []
+        self.feature_expectations_bar = []
+
+    def get_reward(self):
+        obs = 1/(1 + np.exp(-self.obs) + 1e-6)
+        return np.dot(self.weights, obs)
+
+    def train_weights(self, N = 1000):
+        self.feature_expectations.append(self.get_feature_expectation())
+        self.feature_expectations_bar.append(self.feature_expectations[0])
+        self.weights = self.expert_feature_expectation - self.feature_expectations_bar[0]
+
+        for i in range(2, len(self.feature_expectations)):
+            self.Q_table = np.zeros(self.buckets + (self.env.action_space.n,))
+            self.run()
+            self.feature_expectations.append(self.get_feature_expectation())
+            A = self.feature_expectations[i]-self.feature_expectations_bar[i-1]
+            B = self.expert_feature_expectation-self.feature_expectations_bar[i-1]
+            projection = (np.dot(A, B)/np.dot(A, A))*A
+            self.feature_expectations_bar.append(self.feature_expectations_bar[i-1] + projection)
+            self.weights = self.expert_feature_expectation - self.feature_expectations_bar[i]
+            if np.linalg.norm(self.weights) < 1e-5:
+                break
+
+        return self.weights
+
+if __name__ == '__main__':
+    expert = cartpole_agent.CartPoleQLearning()
+    expert.run()
+    expert_feature_expectation = expert.get_feature_expectation()
+    agent = IrlAgent(expert_feature_expectation)
+    agent.train_weights()
+    print(agent.get_reward())
+    print(agent.weights)
+    agent.play()
 
 
 
-class IRL:
 
-    def __init__(self, expert_trajectories, env=agents.Game(), agent=agents.QLearningAgent()):
-        self.expert_trajectories = expert_trajectories
-        self.env = env
-        self.agent = agent
 
-    def get_feature_expectations(self, trajectories):
-        feature_expectations = numpy.zeros(self.agent.feature_size)
-        for trajectory in trajectories:
-            for state, _, _, _ in trajectory:
-                feature_expectations += self.agent.get_features(state)
-        return feature_expectations / len(trajectories)
+
+
+
+
