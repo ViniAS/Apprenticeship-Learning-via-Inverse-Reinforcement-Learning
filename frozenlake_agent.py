28a71ed5d418a7c6b7467dd9644fb8ba5f43dafb
rckalil <b46837@fgv.edu.br>
2024-06-17 15:40:58 -0300

    Doing bayes with lakes

--- frozenlake_agent.py
+++ frozenlake_agent.py
@@ -4,7 +4,7 @@ import math
 import time
 
 class FrozenLakeQLearning:
-    def __init__(self, num_episodes=500, min_lr=0.1, min_epsilon=0.1, discount=1.0, decay=25, render=True):
+    def __init__(self, num_episodes=500, min_lr=0.1, min_epsilon=0.1, discount=1.0, decay=25):
         self.num_episodes = num_episodes
         self.min_lr = min_lr
         self.min_epsilon = min_epsilon
@@ -13,85 +13,94 @@ class FrozenLakeQLearning:
         self.reward = 0
         self.obs = None
 
-        if render:
-            self.env = gym.make('FrozenLake-v1', render_mode='human')
-        else:
-            self.env = gym.make('FrozenLake-v1')
+        self.env = gym.make('FrozenLake-v1', is_slippery=False)
 
-        # This is the action-value function being initialized to 0
+        # Initialize the action-value function to 0
         self.Q_table = np.zeros((self.env.observation_space.n, self.env.action_space.n))
-    
+
     def choose_action(self, state, epsilon):
         return self.env.action_space.sample() if (np.random.random() <= epsilon) else np.argmax(self.Q_table[state])
-    
+
     def update_q(self, state_old, action, reward, state_new, alpha):
         self.Q_table[state_old][action] += alpha * (reward + self.discount * np.max(self.Q_table[state_new]) -
                                                     self.Q_table[state_old][action])
-    
+
     def get_epsilon(self, t):
         return max(self.min_epsilon, min(1., 1. - math.log10((t + 1) / self.decay)))
-    
+
     def get_alpha(self, t):
         return max(self.min_lr, min(1., 1. - math.log10((t + 1) / self.decay)))
-    
-    def run(self, render=True):
+
+    def run(self):
         total_reward = [0] * self.num_episodes
         for t in range(self.num_episodes):
             current_state = self.env.reset()[0]
-            state = current_state
             done = False
+            print(f"Episode {t}")
             while not done:
                 epsilon = self.get_epsilon(t)
-                action = self.choose_action(state, epsilon)
-                obs, reward, done, b, prob = self.env.step(action)
+                action = self.choose_action(current_state, epsilon)
+                obs, reward, done, _, _ = self.env.step(action)
                 new_state = obs
                 if done and reward == 0:
                     reward = -1  # Negative reward for falling into a hole
-                # Penalize if movement hits the edge of the game
-                #if new_state == state:
-                #    reward -= 0.5
+                # if new_state == current_state:
+                #     reward -= 0.5
                 alpha = self.get_alpha(t)
-                self.update_q(state, action, reward, new_state, alpha)
-                state = new_state
+                self.update_q(current_state, action, reward, new_state, alpha)
+                current_state = new_state
                 total_reward[t] += reward
                 if done:
                     break
         return total_reward
-    
+
     def play(self, render=True):
         state = self.env.reset()[0]
-        if render:
-            self.env.render()
         done = False
         while not done:
-            action = np.argmax(self.Q_table[state])
-            obs, reward, done, b, prob= self.env.step(action)
-            state = obs
             if render:
                 self.env.render()
                 time.sleep(0.5)
-        self.env.close()
+            action = np.argmax(self.Q_table[state])
+            obs, reward, done, _, _ = self.env.step(action)
+            state = obs
+            # print(f"Action: {action}, Reward: {reward}")
+            # print(f"State: {state}")
         return reward
-    
+
+    def get_feature_expectation(self, gamma=0.99, num_episodes=100):
+        feature_expectation = np.zeros(self.env.observation_space.n)
+        for _ in range(num_episodes):
+            done = False
+            current_state = self.env.reset()[0]
+            t = 0
+            while not done:
+                action = self.choose_action(current_state, 0)
+                obs, reward, done, _, _ = self.env.step(action)
+                current_state = obs
+                feature_expectation[current_state] += gamma ** t
+                t += 1
+        return feature_expectation / num_episodes
+
     def draw_table(self):
         table = np.zeros((4, 4), dtype=int)
         for i in range(4):
             for j in range(4):
                 table[i][j] = np.argmax(self.Q_table[i * 4 + j])
         print(table)
-    
 
 if __name__ == "__main__":
     start = time.time()
-    agent = FrozenLakeQLearning(num_episodes=500, render=False)
-    total_reward = agent.run()
-    reward = agent.play(render=False)
+    agent = FrozenLakeQLearning(num_episodes=500)
+    agent.run()
     print(f"Time to run: {time.time() - start}")
     print(f"Q_table: {agent.Q_table}")
-    print(f"Reward: {reward}")
+    agent.play(render=False)
+    print("Feature Expectation:")
+    print(agent.get_feature_expectation())
     agent.draw_table()
 
-    m = 10
-    for i in range(m):
+    for _ in range(10):
         reward = agent.play(render=False)
-        print(f"Reward {i+1}: {reward}")
+        print("Reward: ", reward, " for run number ", _)
+    agent.env.close()
---
+++ frozenlake_irl.py
@@ -0,0 +1,125 @@
+import gymnasium as gym
+import numpy as np
+import frozenlake_agent
+import matplotlib.pyplot as plt
+import time
+import torch
+from torch.distributions import Normal
+from torch_sgld import SGLD
+
+class IrlAgent(frozenlake_agent.FrozenLakeQLearning):
+    def __init__(self, expert_feature_expectation, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.expert_feature_expectation = expert_feature_expectation
+        self.weights = np.random.rand(16)
+        self.feature_expectations = []
+        self.feature_expectations_bar = []
+
+    def get_reward(self):
+        obs = 1/(1 + np.exp(-self.obs))
+        return np.dot(self.weights, obs)
+    
+    def train_weights(self, N = 10):
+        self.Q_table = np.zeros((self.env.observation_space.n, self.env.action_space.n))
+        self.feature_expectations.append(self.get_feature_expectation())
+        
+        self.feature_expectations_bar.append(self.feature_expectations[0])
+        self.weights = self.expert_feature_expectation - self.feature_expectations_bar[0]
+        games_lengths = []
+        for i in range(1, N):
+            self.Q_table = np.zeros((self.env.observation_space.n, self.env.action_space.n))
+            games_lengths.append(self.run())
+            self.feature_expectations.append(self.get_feature_expectation())
+            A = self.feature_expectations[i]-self.feature_expectations_bar[i-1]
+            B = self.expert_feature_expectation-self.feature_expectations_bar[i-1]
+            projection = (np.dot(A, B)/np.dot(A, A))*A
+            self.feature_expectations_bar.append(self.feature_expectations_bar[i-1] + projection)
+            self.weights = self.expert_feature_expectation - self.feature_expectations_bar[i]
+            if np.linalg.norm(self.weights) < 1e-5:
+                break
+        
+        return games_lengths
+    
+class IrlAgentBayesian(frozenlake_agent.FrozenLakeQLearning):
+    def __init__(self, expert_feature_expectation, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.expert_feature_expectation = torch.tensor(expert_feature_expectation, dtype=torch.float32)
+        self.weights = np.random.rand(16)
+        self.feature_expectations = []
+        self.feature_expectations_bar = []
+
+    def get_reward(self):
+        obs = 1/(1 + np.exp(-self.obs))
+        return np.dot(self.weights, obs)
+    
+    def train_weights(self, N = 10, lr = 1e-2, prior: torch.distributions.Distribution = Normal(0, 1)):
+        self.Q_table = np.zeros((self.env.observation_space.n, self.env.action_space.n))
+        self.feature_expectations.append(self.get_feature_expectation())
+        
+        weights = prior.sample((16,))
+        weights.requires_grad = True
+        self.weights = weights.detach().numpy()
+        optimizer = SGLD([weights], lr=lr)
+        games_lengths = []
+        losses = []
+
+        for i in range(N):
+            self.Q_table = np.zeros((self.env.observation_space.n, self.env.action_space.n))
+            games_lengths.append(self.run())
+            self.feature_expectations.append(self.get_feature_expectation())
+            loss = torch.norm(weights - self.expert_feature_expectation)**2
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
+
+        return games_lengths, losses
+    
+if __name__ == "__main__":
+    # get time to run
+    time_start = time.time()
+    expert = frozenlake_agent.FrozenLakeQLearning(num_episodes=10)
+    lengths = expert.run()
+
+    expert_feature_expectation = expert.get_feature_expectation()
+    agent = IrlAgentBayesian(expert_feature_expectation)
+
+    games_lengths, losses = agent.train_weights(N=12)
+
+    time_end = time.time()
+
+    # plot results in a 3x4 grid
+    plt.figure()
+    for i in range(12):
+        plt.subplot(3, 4, i+1)
+        plt.plot(games_lengths[i], label='Agent')
+        plt.plot(lengths, label='Expert')
+        plt.title('Game Length vs Episode')
+        plt.xlabel('Episode')
+        plt.ylabel('Game Length')
+        plt.legend()
+
+
+
+    # save plot
+    plt.savefig('irl_cartpole_rewards.png')
+
+    # plot losses
+
+    plt.figure()
+    plt.plot(losses)
+    plt.xlabel('Episode')
+    plt.ylabel('Loss')
+    plt.title('Loss vs Episode')
+    plt.savefig('irl_cartpole_loss.png')
+
+
+
+
+
+
+
+
+
+
+
